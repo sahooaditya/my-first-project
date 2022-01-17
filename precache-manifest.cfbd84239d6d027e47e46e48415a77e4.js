dea7b10eb66cb02f215eff372b97f2d3034ef6e7
Aditya <you@example.com>
2022-01-17 16:39:08 +0530

    Updates

---
+++ precache-manifest.cfbd84239d6d027e47e46e48415a77e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb95ddbaef16718ea9664e1aa391602",
+    "url": "/my-first-project/index.html"
+  },
+  {
+    "revision": "1af6203b903d385237a5",
+    "url": "/my-first-project/static/css/main.cb669b7f.chunk.css"
+  },
+  {
+    "revision": "a1c898fe30a53a98df52",
+    "url": "/my-first-project/static/js/2.3dd7e111.chunk.js"
+  },
+  {
+    "revision": "1546ccc726f110f64aa20141d055c611",
+    "url": "/my-first-project/static/js/2.3dd7e111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af6203b903d385237a5",
+    "url": "/my-first-project/static/js/main.767158b8.chunk.js"
+  },
+  {
+    "revision": "8394a7d7d045f431900f",
+    "url": "/my-first-project/static/js/runtime-main.463e01b0.js"
+  },
+  {
+    "revision": "bf09acc3b984b16ace2b2b8fe03d1354",
+    "url": "/my-first-project/static/media/logo.bf09acc3.svg"
+  }
+]);
